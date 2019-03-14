47ae8ff5c81cec71838d0104be4895d84c8397a0
18JS3026 <18JS3026@L1990>
2019-03-14 13:26:28 +0000

    some stuff done

--- Inventory Management/Inventory Management/Items.vb
+++ Inventory Management/Inventory Management/Items.vb
@@ -1,11 +1,23 @@
 ﻿
 
 Module Items
+    Dim conn As New System.Data.Odbc.OdbcConnection("DRIVER={MySQL ODBC 5.3 ANSI Driver};SERVER=localhost;PORT=3306;DATABASE=itemstuff;USER=root;PASSWORD=password;OPTION=3;")
     Public ItemList As List(Of Item_base) = New List(Of Item_base)()
 
     Sub Main()
 
+        conn.Open()
+        Dim sql As New Odbc.OdbcCommand("CREATE TABLE IF NOT EXISTS `items` (
+  `itemid` int(11) NOT NULL auto_increment,   
+  `itemname` varchar(255) NOT NULL default '',       
+  `itemdesc` varchar(255)  NOT NULL default '',     
+  `itemcost`  int(11) NOT NULL default '0',     
+  `itemmax` int(11)  NOT NULL default '0',    
+  `itemtype`  varchar(255) NOT NULL default '',
+   PRIMARY KEY  (`itemid`)
 
+);")
+        sql.ExecuteNonQuery()
 
 
 
