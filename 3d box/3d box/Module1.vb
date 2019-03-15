2c8f4a76b94e0513afe574bbf1c0c8af07ce2809
18JS3026 <18JS3026@L2240>
2019-03-15 10:56:35 +0000

    ?

---
+++ 3d box/3d box/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MyBox As New box(10, 5, 30)
+        Console.WriteLine(MyBox.GetHeight())
+        Console.WriteLine(MyBox.GetWidth())
+        Console.WriteLine(MyBox.GetLength())
+        Console.WriteLine(MyBox.CalculateSurfaceArea())
+        Console.WriteLine(MyBox.CalculateVolume())
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ 3d box/3d box/box.vb
@@ -0,0 +1,28 @@
+﻿Public Class box
+    Private Height As Double
+    Private Width As Double
+    Private Length As Double
+
+    Public Sub New(Height, Width, Length)
+        Me.Height = Height
+        Me.Width = Width
+        Me.Length = Length
+    End Sub
+    Public Function GetHeight() As Double
+        Return Height
+    End Function
+    Public Function GetWidth() As Double
+        Return Width
+    End Function
+    Public Function GetLength() As Double
+        Return Length
+    End Function
+    Public Function CalculateVolume() As Double
+        Return Length * Width * Height
+    End Function
+    Public Function CalculateSurfaceArea() As Double
+        Return 2 * Length * Height + 2 * Width * Height + 2 * Length * Width
+    End Function
+
+
+End Class
