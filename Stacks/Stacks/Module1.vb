142c9d57160c0db6b5f009f49f92d500e883e765
18JS3026 <18JS3026@L2240>
2019-04-02 15:59:36 +0100

    stacks

---
+++ Stacks/Stacks/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MyStack As New Stack(10)
+        Console.WriteLine(MyStack.IsEmpty)
+        MyStack.Push(5)
+        Console.WriteLine(MyStack.Peek)
+        MyStack.Push(2)
+        Console.WriteLine(MyStack.Pop)
+        MyStack.Push(MyStack.Peek + 5)
+        Console.WriteLine(MyStack.Peek)
+        Console.WriteLine(MyStack.IsEmpty)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        MyStack.Push(MyStack.Peek + 5)
+        Console.WriteLine(MyStack.IsFull)
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Stacks/Stacks/Stack.vb
@@ -0,0 +1,31 @@
+﻿Public Class Stack
+    Private Top As Integer
+    Private Numbers() As Integer
+
+
+    Sub New(ByVal Length As Integer)
+        ReDim Numbers(Length)
+        Top = -1
+    End Sub
+    Public Function Peek() As Integer
+        Return Numbers(Top)
+    End Function
+    Public Function Pop() As Integer
+        Dim PopNum As Integer = Numbers(Top)
+        Top -= 1
+        Return PopNum
+    End Function
+    Public Sub Push(ByVal num As Integer)
+        Top += 1
+        If IsFull() Then
+            Exit Sub
+        End If
+        Numbers(Top) = num
+    End Sub
+    Public Function IsEmpty() As Boolean
+        Return Top = -1
+    End Function
+    Public Function IsFull() As Boolean
+        Return Numbers.Length = Top
+    End Function
+End Class
