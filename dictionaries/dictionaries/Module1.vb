813aa30dd4d251c6b47b9ac7705eff91ddd24810
18JS3026 <18JS3026@L2240>
2019-05-14 15:52:06 +0100

    aaa

---
+++ dictionaries/dictionaries/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myDictionary As New Dictionary(Of String, Integer)
+        Dim UserInput As String
+        UserInput = Console.ReadLine()
+        Dim SplitInput() As String = UserInput.Split(" ")
+        For Each k As String In SplitInput
+            If myDictionary.ContainsKey(k) Then
+                myDictionary(k) += 1
+            Else
+                myDictionary.Add(k, 1)
+            End If
+        Next
+        For Each item In myDictionary.Keys
+            Console.WriteLine($"{item} | {myDictionary(item)}")
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
