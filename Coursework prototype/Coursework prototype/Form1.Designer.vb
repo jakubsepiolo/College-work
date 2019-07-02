742014ee24a03600f9d2ed80f9d9d9695e8c37fd
18JS3026 <18JS3026@L2470>
2019-07-02 12:21:33 +0100

    almost finished

--- Coursework prototype/Coursework prototype/Form1.Designer.vb
+++ Coursework prototype/Coursework prototype/Form1.Designer.vb
@@ -26,6 +26,11 @@ Partial Class Form1
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -44,11 +49,11 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(248, 95)
+        Me.Button1.Location = New System.Drawing.Point(277, 50)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Add"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox3
@@ -59,11 +64,62 @@ Partial Class Form1
         Me.TextBox3.Size = New System.Drawing.Size(258, 20)
         Me.TextBox3.TabIndex = 3
         '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(277, 79)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Subtract"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(277, 108)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Multiply"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(277, 137)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Divide"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(65, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(135, 26)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Input complex number here" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "In form a+bi"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(394, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 18)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Result: "
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox2)
@@ -79,4 +135,9 @@ Partial Class Form1
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- Coursework prototype/Coursework prototype/Form1.vb
+++ Coursework prototype/Coursework prototype/Form1.vb
@@ -1,38 +1,86 @@
-﻿Public Class Form1
+﻿
+Public Class Form1
+    'todo: make input of "3+i" detect complex part as 1 (no need for 3+1i to be input)
+    Public Function StringToComplexNumber(ByVal Text As String) As ComplexNumber
+        Dim IsNegativeComplex As Boolean = False
+        Dim IsNegativeReal As Boolean = False
+        Dim ComplexOne As String
+        Dim RealOne As String
+        For i = 0 To Len(Text) - 2
+
+            If InStr(Text, "+") = 0 Then
+                If InStr(Text, "-") <> 1 Then ' if negative symbol in string then follow this
+                    If i > InStr(Text, "-") - 1 Then
+                        ComplexOne = ComplexOne & Text(i)
+                    ElseIf i < InStr(Text, "-") - 1 Then
+                        RealOne = RealOne & Text(i)
+                    End If
+                    IsNegativeComplex = True
+                ElseIf InStrRev(Text, "-") <> 1 Then
+                    If i > InStrRev(Text, "-") - 1 Then
+                        ComplexOne = ComplexOne & Text(i)
+                    ElseIf i < InStrRev(Text, "-") - 1 Then
+                        RealOne = RealOne & Text(i)
+                    End If
+                    IsNegativeComplex = True
+                Else
+                    If i > InStr(Text, "+") - 1 Then
+                        ComplexOne = ComplexOne & Text(i)
+                    ElseIf i < InStr(Text, "+") - 1 Then
+                        RealOne = RealOne & Text(i)
+                    End If
+                    IsNegativeComplex = False
+                End If
+            Else
+                If InStr(Text, "+") <> 1 Then
+                    If i > InStr(Text, "+") - 1 Then
+                        ComplexOne = ComplexOne & Text(i)
+                    ElseIf i < InStr(Text, "+") - 1 Then
+                        RealOne = RealOne & Text(i)
+                    End If
+                    IsNegativeReal = False
+                ElseIf InStrRev(Text, "+") <> 1 Then
+                    If i > InStrRev(Text, "+") - 1 Then
+                        ComplexOne = ComplexOne & Text(i)
+                    ElseIf i < InStrRev(Text, "+") - 1 Then
+                        RealOne = RealOne & Text(i)
+                    End If
+                    IsNegativeReal = False
+                Else
+                    If i > InStr(Text, "-") - 1 Then
+                        ComplexOne = ComplexOne & Text(i)
+                    ElseIf i < InStr(Text, "-") - 1 Then
+                        RealOne = RealOne & Text(i)
+                    End If
+                    IsNegativeReal = True
+                End If
+            End If
 
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Next
+        Try
+            Dim FinalNumber As New ComplexNumber(Convert.ToSingle(RealOne), Convert.ToSingle(ComplexOne))
+            If IsNegativeComplex Then
+                FinalNumber.Complex = -FinalNumber.Complex
+            End If
+            If IsNegativeReal Then
+                FinalNumber.Real = -FinalNumber.Real
+            End If
+            Return FinalNumber
+        Catch ex As FormatException
+            MsgBox("Numbers not input in correct format!")
+            Return New ComplexNumber(0, 0)
+        Catch ex As Exception
+            MsgBox("Error: " & ex.Message)
+            Return New ComplexNumber(0, 0)
+        End Try
+    End Function
+
+    Private Sub Add_Button(sender As Object, e As EventArgs) Handles Button1.Click
         Dim FirstInput As String = TextBox1.Text
         Dim SecondInput As String = TextBox2.Text
-        Dim RealOne As String
-        Dim ComplexOne As String
-        Dim RealTwo As String
-        Dim ComplexTwo As String
-
-        For i = 0 To Len(FirstInput) - 2
-
-            If i > InStr(FirstInput, "+") - 1 Then
-                'Or i > InStr(FirstInput, "-")' Then
-                ComplexOne = ComplexOne & FirstInput(i)
-            ElseIf i < InStr(FirstInput, "+") - 1 Then
-                'Or i < InStr(FirstInput, "-") Then
-                RealOne = RealOne & FirstInput(i)
-            End If
-
-        Next
-        Dim NumberOne As New ComplexNumber(Convert.ToSingle(RealOne), Convert.ToSingle(ComplexOne))
-
-        For i = 0 To Len(SecondInput) - 2
-            If i > InStr(SecondInput, "+") - 1 Then
-                'Or i > InStr(SecondInput, "-") Then
-                ComplexTwo = ComplexTwo & SecondInput(i)
-            ElseIf i < InStr(SecondInput, "+") - 1 Then
-                'Or i < InStr(SecondInput, "-") Then
-                RealTwo = RealTwo & SecondInput(i)
-            End If
-
-        Next
-        Dim NumberTwo As New ComplexNumber(Convert.ToSingle(RealTwo), Convert.ToSingle(ComplexTwo))
+        Dim NumberOne As ComplexNumber = StringToComplexNumber(FirstInput)
+        Dim NumberTwo As ComplexNumber = StringToComplexNumber(SecondInput)
         Dim Total As ComplexNumber
         Total = NumberOne + NumberTwo
         If Total.Complex > 0 Then
@@ -46,7 +94,67 @@
 
     End Sub
 
-    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub Subtract_Button(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim FirstInput As String = TextBox1.Text
+        Dim SecondInput As String = TextBox2.Text
+        Dim NumberOne As ComplexNumber = StringToComplexNumber(FirstInput)
+        Dim NumberTwo As ComplexNumber = StringToComplexNumber(SecondInput)
+        Dim Total As ComplexNumber
+        Total = NumberOne - NumberTwo
+        If Total.Complex > 0 Then
+            TextBox3.Text = Total.Real & "+" & Total.Complex & "i"
+        ElseIf Total.Complex = 0 Then
+            TextBox3.Text = Total.Real
+        Else
+
+            TextBox3.Text = Total.Real & Total.Complex & "i"
+        End If
+
+    End Sub
+
+    Private Sub Multiply_Button(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim FirstInput As String = TextBox1.Text
+        Dim SecondInput As String = TextBox2.Text
+        Dim NumberOne As ComplexNumber = StringToComplexNumber(FirstInput)
+        Dim NumberTwo As ComplexNumber = StringToComplexNumber(SecondInput)
+        Dim Total As ComplexNumber
+        Total = NumberOne * NumberTwo
+        If Total.Complex > 0 Then
+            TextBox3.Text = Total.Real & "+" & Total.Complex & "i"
+        ElseIf Total.Complex = 0 Then
+            TextBox3.Text = Total.Real
+        Else
+
+            TextBox3.Text = Total.Real & Total.Complex & "i"
+        End If
+
+    End Sub
+
+    Private Sub Divide_Button(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim FirstInput As String = TextBox1.Text
+        Dim SecondInput As String = TextBox2.Text
+        Dim NumberOne As ComplexNumber = StringToComplexNumber(FirstInput)
+        Dim NumberTwo As ComplexNumber = StringToComplexNumber(SecondInput)
+        Dim Total As ComplexNumber
+        Total = NumberOne / NumberTwo
+        If Total.Complex > 0 Then
+            TextBox3.Text = Total.Real & "+" & Total.Complex & "i"
+        ElseIf Total.Complex = 0 Then
+            TextBox3.Text = Total.Real
+        Else
+
+            TextBox3.Text = Total.Real & Total.Complex & "i"
+        End If
+
+    End Sub
+
+
 End Class
