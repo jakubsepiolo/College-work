feff79d682bddf353cedb21e4b3c86343d537db1
18JS3026 <18JS3026@L2135>
2019-11-19 11:44:21 +0000

    asdasgadgd

--- Coursework prototype/Coursework prototype/Form4.vb
+++ Coursework prototype/Coursework prototype/Form4.vb
@@ -1,20 +1,22 @@
 ﻿Imports System.Text.RegularExpressions
 Public Class Form4
     'no more flickering but need tidy code
-    Private GraphScale As Decimal = 20
+    Private GraphScale As Decimal = 1
     Dim PixelPerPoint As Integer = 15
     Private Points As New List(Of ComplexNumber)
     Public Inequalities As New List(Of String)
     Dim AxisFont As New Font("Arial", 6, FontStyle.Regular)
     Dim PointFont As New Font("Arial", 12, FontStyle.Regular)
-    Dim ShowLabels As CheckBox = New CheckBox() With {.Text = "Display axis labels", .Checked = True}
-    Dim ShowLoci As CheckBox = New CheckBox() With {.Text = "Display Loci", .Checked = False}
+    Friend WithEvents ShowLabels As CheckBox = New CheckBox() With {.Text = "Display axis labels", .Checked = True}
+    Friend WithEvents ShowLoci As CheckBox = New CheckBox() With {.Text = "Display Loci", .Checked = False}
+    Friend WithEvents ShowComplexPoints As CheckBox = New CheckBox() With {.Text = "Display Complex points", .Checked = False}
     Dim PenList() As Pen = {Pens.Black, Pens.Blue, Pens.Green, Pens.Firebrick, Pens.Violet, Pens.Moccasin}
 
 
     Private Sub Form4_Paint(ByVal sender As Object, ByVal e As PaintEventArgs) Handles Me.Paint
         ShowLabels.Location = New Point(2, Height - 80)
         ShowLoci.Location = New Point(2, Height - 60)
+        ShowComplexPoints.Location = New Point(2, Height - 100)
         LociButton.Location = New Point(Width - 75, Height - 70)
         Dim AxisPen As New Pen(Color.FromArgb(255, 255, 0, 0))
         Dim X As Integer = Width \ 2 + 6
@@ -35,32 +37,31 @@ Public Class Form4
             For f = 0 To Inequalities.Count - 1
                 Dim EquationList As List(Of String)() = InequalityToEquation(Inequalities(f))
                 Dim Offset As Integer = Width \ 2
-                Dim G As Graphics = CreateGraphics()
-                Dim Multiplier As Integer = 10
+                Dim Multiplier As Integer = 50
 
                 For i = 0 To Width Step 5
                     Dim a As Single
                     Dim b As Single
                     Try
-                        a = ((i - Offset) / PixelPerPoint) ^ 4 * EquationList(0)(0) + ((i - Offset) / PixelPerPoint) ^ 3 * EquationList(0)(1) + ((i - Offset) / PixelPerPoint) ^ 2 * EquationList(0)(2) + EquationList(0)(3) * ((i - Offset) / PixelPerPoint) + EquationList(0)(4)
-                        b = ((i - Offset) / PixelPerPoint) ^ 4 * EquationList(1)(0) + ((i - Offset) / PixelPerPoint) ^ 3 * EquationList(1)(1) + ((i - Offset) / PixelPerPoint) ^ 2 * EquationList(1)(2) + EquationList(1)(3) * ((i - Offset) / PixelPerPoint) + EquationList(1)(4)
+                        a = (((i - Offset) / PixelPerPoint) * GraphScale) ^ 4 * EquationList(0)(0) + (((i - Offset) / PixelPerPoint) * GraphScale) ^ 3 * EquationList(0)(1) + (((i - Offset) / PixelPerPoint) * GraphScale) ^ 2 * EquationList(0)(2) + EquationList(0)(3) * (((i - Offset) / PixelPerPoint) * GraphScale) + EquationList(0)(4)
+                        b = (((i - Offset) / PixelPerPoint) * GraphScale) ^ 4 * EquationList(1)(0) + (((i - Offset) / PixelPerPoint) * GraphScale) ^ 3 * EquationList(1)(1) + (((i - Offset) / PixelPerPoint) * GraphScale) ^ 2 * EquationList(1)(2) + EquationList(1)(3) * (((i - Offset) / PixelPerPoint) * GraphScale) + EquationList(1)(4)
                     Catch ex As InvalidCastException
                         MsgBox($"One or more of your equations was invalid {vbCrLf}{vbCrLf}{ex.Message}{vbCrLf}{vbCrLf}Please input your equations again")
                         Exit For
                     End Try
-                    For j = 0 To Height Step 15
+                    For j = 0 To Height Step 5
                         If a > b And EquationList(2)(0) = ">" Then
                             Dim aa As Single = j
                             Dim xx As Single = i
-                            G.DrawEllipse(PenList(f), xx, aa + f * 5, 1, 1)
+                            e.Graphics.DrawEllipse(PenList(f), xx, aa + f * 2, 1, 1)
                         ElseIf a < b And EquationList(2)(0) = "<" Then
                             Dim aa As Single = j
                             Dim xx As Single = i
-                            G.DrawEllipse(PenList(f), xx, aa + f * 5, 1, 1)
+                            e.Graphics.DrawEllipse(PenList(f), xx, aa + f * 2, 1, 1)
                         ElseIf a = b And EquationList(2)(0) = "=" Then
                             Dim aa As Single = j
                             Dim xx As Single = i
-                            G.DrawEllipse(PenList(f), xx, aa + f * 5, 1, 1)
+                            e.Graphics.DrawEllipse(PenList(f), xx, aa + f * 2, 1, 1)
                         End If
                     Next
                 Next
@@ -68,8 +69,8 @@ Public Class Form4
                     Dim a As Single
                     Dim b As Single
                     Try
-                        a = (((i / Multiplier) - Offset) / PixelPerPoint) ^ 4 * EquationList(0)(0) + (((i / Multiplier) - Offset) / PixelPerPoint) ^ 3 * EquationList(0)(1) + (((i / Multiplier) - Offset) / PixelPerPoint) ^ 2 * EquationList(0)(2) + EquationList(0)(3) * (((i / Multiplier) - Offset) / PixelPerPoint) + EquationList(0)(4)
-                        b = (((i / Multiplier) - Offset) / PixelPerPoint) ^ 4 * EquationList(1)(0) + (((i / Multiplier) - Offset) / PixelPerPoint) ^ 3 * EquationList(1)(1) + (((i / Multiplier) - Offset) / PixelPerPoint) ^ 2 * EquationList(1)(2) + EquationList(1)(3) * (((i / Multiplier) - Offset) / PixelPerPoint) + EquationList(1)(4)
+                        a = ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) ^ 4 * EquationList(0)(0) + ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) ^ 3 * EquationList(0)(1) + ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) ^ 2 * EquationList(0)(2) + EquationList(0)(3) * ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) + EquationList(0)(4)
+                        b = ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) ^ 4 * EquationList(1)(0) + ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) ^ 3 * EquationList(1)(1) + ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) ^ 2 * EquationList(1)(2) + EquationList(1)(3) * ((((i / Multiplier) - Offset) / PixelPerPoint) * GraphScale) + EquationList(1)(4)
                     Catch ex As InvalidCastException
                         Inequalities.Clear()
                         Exit For
@@ -77,11 +78,21 @@ Public Class Form4
                     Dim aa As Single = (Height / 2) - PixelPerPoint * a * (1 / GraphScale)
                     Dim bb As Single = (Height / 2) - PixelPerPoint * b * (1 / GraphScale)
                     Dim xx As Single = (i / Multiplier)
-                    G.DrawEllipse(PenList(f), xx, aa, 1, 2)
-                    G.DrawEllipse(PenList(f), xx, bb, 1, 2)
+                    If aa > Height Then
+                        aa = Width + 100
+                    ElseIf aa < 0 Then
+                        aa = -50
+
+                    End If
+                    If bb > Height Then
+                        bb = Height + 100
+                    ElseIf bb < 0 Then
+                        bb = -50
+                    End If
+                    e.Graphics.DrawEllipse(PenList(f), xx, aa, 1, 2)
+                    e.Graphics.DrawEllipse(PenList(f), xx, bb, 1, 2)
                 Next
             Next
-
         End If
         For i = 1 To loopcount
 
@@ -93,25 +104,28 @@ Public Class Form4
                 e.Graphics.DrawString(((-(Width \ (PixelPerPoint * 2)) - 1 + i) * GraphScale), AxisFont, Brushes.Black, XOffset + PixelPerPoint * i, Height \ 2)
             End If
         Next
-        For i = 0 To Points.Count - 1
-            Dim Xb As Integer = NumbersToCoordinate(Points(i))(0)
-            Dim yb As Integer = NumbersToCoordinate(Points(i))(1)
-            Dim Number() As Decimal = CoordinatesToNumber(Xb, yb)
-            Dim Complex As New ComplexNumber(Number(0), Number(1))
-            Dim Modulus As Single = (Complex.Modulus) * (PixelPerPoint / 2) * (1 / GraphScale)
+        If ShowComplexPoints.Checked Then
+            For i = 0 To Points.Count - 1
+                Dim Xb As Integer = NumbersToCoordinate(Points(i))(0)
+                Dim yb As Integer = NumbersToCoordinate(Points(i))(1)
+                Dim Number() As Decimal = CoordinatesToNumber(Xb, yb)
+                Dim Complex As New ComplexNumber(Number(0), Number(1))
+                Dim Modulus As Single = (Complex.Modulus) * (PixelPerPoint / 2) * (1 / GraphScale)
 
-            Dim mybrush = New SolidBrush(Color.FromArgb(255, 0, 0, 0))
+                Dim mybrush = New SolidBrush(Color.FromArgb(255, 0, 0, 0))
 
-            Xb = NumbersToCoordinate(Complex)(0)
-            yb = NumbersToCoordinate(Complex)(1)
+                Xb = NumbersToCoordinate(Complex)(0)
+                yb = NumbersToCoordinate(Complex)(1)
 
-            e.Graphics.DrawEllipse(Pens.Blue, (Xb - 3), (yb - 4), 5, 5)
-            Dim A As Single = -(Complex.Argument * 180 / 3.14)
+                e.Graphics.DrawEllipse(Pens.Blue, (Xb - 3), (yb - 4), 5, 5)
+                Dim A As Single = -(Complex.Argument * 180 / 3.14)
 
-            e.Graphics.DrawLine(Pens.Black, Width \ 2, Height \ 2, Xb, yb)
-            e.Graphics.DrawArc(Pens.Black, Width \ 2 - Modulus \ 2, Height \ 2 - Modulus \ 2, Modulus, Modulus, 0, A)
-            e.Graphics.DrawString(Points(i).Real & " " & Points(i).Complex & "i" & vbCrLf & Points(i).ModulusArgument, PointFont, Brushes.Black, NumbersToCoordinate(Points(i))(0), NumbersToCoordinate(Points(i))(1))
-        Next
+                e.Graphics.DrawLine(Pens.Black, Width \ 2, Height \ 2, Xb, yb)
+                e.Graphics.DrawArc(Pens.Black, Width \ 2 - Modulus \ 2, Height \ 2 - Modulus \ 2, Modulus, Modulus, 0, A)
+                e.Graphics.DrawString(Points(i).Real & " " & Points(i).Complex & "i" & vbCrLf & Points(i).ModulusArgument, PointFont, Brushes.Black, NumbersToCoordinate(Points(i))(0), NumbersToCoordinate(Points(i))(1))
+            Next
+        End If
+
 
     End Sub
 
@@ -212,9 +226,11 @@ Public Class Form4
         Select Case e.Button
             Case MouseButtons.Left
 
+                If ShowComplexPoints.Checked Then
+                    Points.Add(New ComplexNumber(CoordinatesToNumber(e.X, e.Y)(0), CoordinatesToNumber(e.X, e.Y)(1)))
+                    Invalidate()
+                End If
 
-                Points.Add(New ComplexNumber(CoordinatesToNumber(e.X, e.Y)(0), CoordinatesToNumber(e.X, e.Y)(1)))
-                Invalidate()
             Case MouseButtons.Right
                 Dim prompt As DialogResult = MessageBox.Show("Are you sure you want to clear the graph?", "Are you sure?", MessageBoxButtons.YesNo)
 
@@ -250,7 +266,7 @@ Public Class Form4
 
 
     Private Sub Form4_Displayed(sender As Object, e As EventArgs) Handles Me.Shown
-        Invalidate()
+        'Invalidate()
         Randomize()
         DoubleBuffered = True
         'For i = 0 To 10
@@ -261,7 +277,11 @@ Public Class Form4
     Private Sub Form4_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
         Controls.Add(ShowLabels)
         Controls.Add(ShowLoci)
+        Controls.Add(ShowComplexPoints)
 
+    End Sub
+    Private Sub CheckedChanged(sender As Object, e As EventArgs) Handles ShowComplexPoints.CheckedChanged, ShowLoci.CheckedChanged, ShowLabels.CheckedChanged
+        Invalidate()
     End Sub
 
     Public Sub GraphInequality(Line1 As String, Line2 As String, Inequality As String)
